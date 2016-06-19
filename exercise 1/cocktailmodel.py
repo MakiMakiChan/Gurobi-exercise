456e598e31173ce5a48d5713ac9117daebc2b892
TimmM <timm.meiwes@rwth-aachen.de>
2016-06-19 16:11:55 +0200

    erste Versuche der Lösung funktioniert noch nicht

--- exercise 1/cocktailmodel.py
+++ exercise 1/cocktailmodel.py
@@ -16,17 +16,21 @@ def solve(ingredients, prices, alcohol, sugar, calories, min_alcohol, max_alcoho
         #!!! x[i] entspricht der Menge (Volumen), die von Inhaltsstoff i zum Mischen des Cocktails verwendet wird (in 100 ml) !!!
         #!!! Aenderungen des Namens bedeuten 0 Punkte; Changing the name results in getting 0 points !!!
         # ergänzen Sie ggf. untere und obere Schranken, sowie Zielfunktionskoeffizienten
-        x[i] = model.addVar(name="x_%s" % i)
-
-    model.update()
+        x[i] = model.addVar(name="x_%s" % i, obj = prices[i]) 
+  
+   
     
+    model.update()
+    model.setObjective(x[i] * min_volume * prices[i], GRB.MINIMIZE) 
     # ergaenzt hier die linearen Nebenbedingungen (aka constraints):
     # !!! Nichtlinearitaeten,im Modell z.B. Multiplikation zweier Entscheidungsvariablen fuehren zu 0 Punkten !!!
     # !!! nonlinearities in the model, e.g., multiplication of two decision variables, result in getting 0 points !!!
-    
-    
-    
-    
+    model.addConstr(quicksum(alcohol[i] for i in alcohol) >= min_alcohol)
+   # model.addConstr(quicksum(alcohol[i] for i in alcohol) <= max_alcohol)
+    model.addConstr(quicksum(sugar[i] for i in sugar) >= min_sugar)
+  #  model.addConstr(quicksum(calories[i] for i in calories) <= max_calories)
+   # model.addConstr(quicksum(vol))
+    model.update()
     
     
     
